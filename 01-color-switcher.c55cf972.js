03a753fa7a87b67b2022a9a1714fdd764ce05b37
melyasanya <127304495+melyasanya@users.noreply.github.com>
2023-05-05 21:56:51 +0000

    Deploying to gh-pages from @ melyasanya/goit-js-hw-09@51007551acf97f8ab09f6540a8606ccfbfb58286 🚀

--- 01-color-switcher.c55cf972.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;e.addEventListener("click",(function(){a=setInterval((function(){t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))),e.disabled=!0}),1e3)})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.c55cf972.js.map
